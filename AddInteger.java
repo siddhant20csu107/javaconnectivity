33a076a47bd9d0a10c1bcbef22fb5a4d6841620d
siddhant20csu107 <siddhant20csu107@ncuindia.edu>
2023-03-20 00:49:47 +0530

    Creating Java Connection

---
+++ AddInteger.java
@@ -0,0 +1,10 @@
+class AddInteger{
+    public static void main(String[] args){
+        System.out.println("Enter two numbers:");
+        int first = 10, second = 20;
+        System.out.println(first+" "+second);
+        //add two numbers
+        int sum = first+second;
+        System.out.println("The sum is:"+sum);
+    }
+}
